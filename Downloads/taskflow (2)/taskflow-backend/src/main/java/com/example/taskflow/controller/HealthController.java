a06064926b11baff713e5f5098d0ab79cc360781
nandininaidu <nandini2005729gmail.com>
2026-01-12 20:45:18 +0530

    added first api endpoint

---
+++ Downloads/taskflow (2)/taskflow-backend/src/main/java/com/example/taskflow/controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.example.taskflow.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "TaskFlow backend is running";
+    }
+}
